71034309b43d837d289a8fd1087c6b66df3533ee
Raphaël Vinot <raphael@vinot.info>
2020-09-07 14:45:53 +0200

    [do not merge] incomplete support for google fonts, not usable (no versioning, many filetypes not present)

--- sanejs/sanejs.py
+++ sanejs/sanejs.py
@@ -10,31 +10,24 @@ from git import Repo  # type: ignore
 
 from .helpers import get_homedir, get_socket_path
 
-"""
-sha: set of libname|version|fullpath
 
-libname|version: hash of fullpath -> sha
-"""
+class CdnJS():
 
-# We assume the initialisation of the submodule is done before calling this class.
-
-
-class SaneJS():
-
-    def __init__(self, loglevel: int=logging.DEBUG) -> None:
+    def __init__(self, redis: Redis, loglevel: int):
         self.__init_logger(loglevel)
-        self.libs_path = get_homedir() / 'cdnjs' / 'ajax' / 'libs'
-        self.redis_lookup = Redis(unix_socket_path=get_socket_path('lookup'), decode_responses=True)
-        self.cdnjs_repo = Repo(str(get_homedir() / 'cdnjs'))
+        self.redis = redis
+        self.repo_path = get_homedir() / 'cdnjs'
+        self.repo = Repo(str(self.repo_path))
+        self.libs_path = self.repo_path / 'ajax' / 'libs'
 
     def __init_logger(self, loglevel: int):
         self.logger = logging.getLogger(f'{self.__class__.__name__}')
         self.logger.setLevel(loglevel)
 
-    def _pull_dnsjs(self):
-        last_commit_ts = self.redis_lookup.get('last_commit')
+    def _pull(self):
+        last_commit_ts = self.redis.get('last_commit')
         if not last_commit_ts or int(last_commit_ts) < time.time() - 10000:
-            self.cdnjs_repo.remote('origin').pull()
+            self.repo.remote('origin').pull()
             return True
         return False
 
@@ -42,8 +35,8 @@ class SaneJS():
         '''Compute the hashes for the (new) files, create a file in the root directory of each library'''
         if force_recompute:
             self.logger.info('Force recompute and re-cache everything.')
-            self.redis_lookup.flushdb()
-        if not self._pull_dnsjs():
+            self.redis.flushdb()
+        if not self._pull():
             return
         self.logger.debug('Compute hashes.')
         for libname in self.libs_path.iterdir():
@@ -53,7 +46,7 @@ class SaneJS():
             short_libname = libname.as_posix().replace(self.libs_path.as_posix() + '/', '')
             self.logger.info(f'Processing {short_libname}.')
             all_hashes_lib = {}
-            p = self.redis_lookup.pipeline()
+            p = self.redis.pipeline()
             p.sadd('all_libraries', short_libname)
             for version in libname.iterdir():
                 # This is the directory for a version of the library. It can contain all kind of directories and files
@@ -110,5 +103,75 @@ class SaneJS():
                 # Write a file with all the hashes for all the versions at the root directory of the library
                 json.dump(all_hashes_lib, f, indent=2)
             p.execute()
-        self.redis_lookup.set('ready', 1)
+        self.redis.set('ready', 1)
         self.logger.debug('Compute hashes done.')
+
+
+class GoogleFonts():
+
+    def __init__(self, redis: Redis, loglevel: int):
+        self.__init_logger(loglevel)
+        self.redis = redis
+        self.repo_path = get_homedir() / 'google-fonts'
+        self.repo = Repo(str(self.repo_path))
+        self.licenses = ['apache', 'ofl', 'ufl']
+
+    def __init_logger(self, loglevel: int):
+        self.logger = logging.getLogger(f'{self.__class__.__name__}')
+        self.logger.setLevel(loglevel)
+
+    def _pull(self):
+        last_commit_ts = self.redis.get('last_commit')
+        if not last_commit_ts or int(last_commit_ts) < time.time() - 10000:
+            self.repo.remote('origin').pull()
+            return True
+        return False
+
+    def compute_hashes(self, force_recompute: bool=False):
+        '''Compute the hashes for the (new) files, create a file in the root directory of each library'''
+        if force_recompute:
+            self.logger.info('Force recompute and re-cache everything.')
+            self.redis.flushdb()
+        if not self._pull():
+            return
+        self.logger.debug('Compute hashes.')
+        for license in self.licenses:
+            path = self.repo_path / license
+            for font in path.iterdir():
+                # font is the path to the font directory, each irectory contains one or more ttf files
+                if not path.is_dir():
+                    continue
+                short_fontname = font.as_posix().replace(path.as_posix() + '/', '')
+                self.logger.info(f'Processing {short_fontname}.')
+                all_hashes_font = {}
+                p = self.redis.pipeline()
+                p.sadd('all_libraries', short_fontname)
+
+                for font_style in font.glob('**/*.ttf'):
+                    # We only care about the ttf files
+                    short_font_style = font_style.as_posix().replace(font.as_posix() + '/', '')
+                    if force_recompute or not (font / 'hashes.json').exists():
+                        # Only compute the *new* hashes (unless specified)
+                        to_save = {}
+                        with open(font_style, 'rb') as f_to_h:
+                            content = f_to_h.read()
+                        file_hash_default = hashlib.sha512(content)
+                        to_save[short_font_style] = {'default': file_hash_default.hexdigest()}
+                        p.sadd(file_hash_default.hexdigest(), f'{license}|{short_fontname}|{short_font_style}')
+                        p.hset(f'{license}|{short_fontname}', short_font_style, file_hash_default.hexdigest())
+                        p.sadd(license, short_fontname)
+
+
+
+class SaneJS():
+
+    def __init__(self, loglevel: int=logging.DEBUG) -> None:
+        self.__init_logger(loglevel)
+        self.google_fonts_repo_path = get_homedir() / 'google-fonts'
+        self.redis_lookup = Redis(unix_socket_path=get_socket_path('lookup'), decode_responses=True)
+        self.google_fonts_repo = Repo(str(get_homedir() / 'cdnjs'))
+
+    def __init_logger(self, loglevel: int):
+        self.logger = logging.getLogger(f'{self.__class__.__name__}')
+        self.logger.setLevel(loglevel)
+
